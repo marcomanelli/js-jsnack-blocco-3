45fe398a119228ea29d931576a02dd4d5ab0ba3b
marcomanelli <tronmsi@gmail.com>
2021-04-27 18:17:14 +0200

    fix-6

--- snack-6/js/script.js
+++ snack-6/js/script.js
@@ -5,16 +5,16 @@ $(function(){
   var arr1 = [1, 5, 6, 2, 7, 3]
   var arr2 = [8, 3, 9, 2, 8, 2, 5, 2, 1]
 
-  while(arr1.length != arr2.lenght){
+  while(arr1.length != arr2.length){
 
-    if(arr1.length < arr2.lenght){
+    if(arr1.length < arr2.length){
       arr1.push(generateRandomNumber10())
     }else{
       arr2.push(generateRandomNumber10())
     }
   }
 
-  console.log(arr1.length, arr2.lenght)
+  console.log(arr1.length, arr2.length)
 
   function generateRandomNumber10() {
     return Math.floor((Math.random() * 10))
